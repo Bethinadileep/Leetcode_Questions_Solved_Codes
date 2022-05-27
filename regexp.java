73f667a5cb1ac54e03323526e4bd4326a81e57b8
Bethina Dileep <56349269+Bethinadileep@users.noreply.github.com>
2022-05-27 08:00:30 +0530

    Create regexp.java

---
+++ regexp.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		while (true) {
+		    System.out.println("Enter regex Pattern: ");
+		    Pattern pattern = Pattern.compile(sc.nextLine());
+		    System.out.println("Enter Text:");
+		    Matcher matcher = pattern.matcher(sc.nextLine());
+		    boolean found = false;
+		    while(matcher.find()) {
+		        System.out.println("I found the text "+matcher.groupmatcher.start() + " and ending at index "+matcher.end());
+		        found = true;
+		    }
+		    if(!found){
+		        System.out.println("No match found");
+		    }
+		}
+	}
+}
